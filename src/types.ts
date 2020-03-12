28f728c1c4536fe201b6b0a8abf05f18886895c1
Anthony Morris <anthony@amorrissound.com>
2020-03-12 01:07:53 -0700

    Cleanup redundant type interface

--- src/types.ts
+++ src/types.ts
@@ -3,15 +3,3 @@ import { Firestore } from '@google-cloud/firestore';
 export interface Context {
   db: Firestore;
 }
-
-export interface Article {
-  title: string;
-  subtitle: string;
-  content: string;
-  headerImageURL: string;
-  authorId: string;
-  categories: Array<string>;
-  comments: Array<string>;
-  likes: number;
-  wordLength: number;
-}
