9b7beb57f7921d24cef4eed285e216bb236b4920
Anthony Morris <anthony@amorrissound.com>
2020-03-12 10:43:38 -0700

    Add typing for resolvers

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,5 +1,8 @@
+import { IResolvers } from '../generated/graphql';
 import merge from 'lodash.merge';
 
 import articles from './articles';
 
-export default merge(articles);
+const resolvers: IResolvers = merge(articles);
+
+export default resolvers;
