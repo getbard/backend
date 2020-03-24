65e2366e9ef8aed1bcc845759b595eaf291f2f6e
Anthony Morris <anthony@amorrissound.com>
2020-03-24 11:04:22 -0700

    Fix resolvers type issue

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,12 +1,9 @@
-import { IResolvers } from '../generated/graphql';
+import { IResolvers, ResolversObject } from '../generated/graphql';
 import merge from 'lodash.merge';
 
 import articles from './articles';
 import users from './users';
 
-// @TODO: Resolve this issue!
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
-const resolvers: IResolvers = merge(articles, users);
+const resolvers: IResolvers = merge(articles, users) as ResolversObject<any>;
 
 export default resolvers;
