d2c4f2e5f5531293092ef0a85bca8e4516fe0b79
Anthony Morris <anthony@amorrissound.com>
2020-04-17 14:34:21 -0700

    Implement updating a user

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,9 @@
-import { firestore, auth } from 'firebase-admin';
+import { firestore, app } from 'firebase-admin';
 import stream from 'getstream';
 
 export interface Context {
   db: firestore.Firestore;
   userId: string | null;
   stream: stream.StreamClient;
-  auth: auth.Auth;
+  firebase: app.App;
 }
