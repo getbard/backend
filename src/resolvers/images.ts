82e0e29b0277ebff8723595bbe5420a155fd5b35
Anthony Morris <anthony@amorrissound.com>
2020-07-03 21:05:47 -0700

    Handle edge case for files with () in them

--- src/resolvers/images.ts
+++ src/resolvers/images.ts
@@ -27,7 +27,9 @@ const uploadImage = async (
   // Initialize GCP Cloud Storage
   const storeageClient = storage();
   const bucket = storeageClient.bucket(process.env.IMAGE_BUCKET_NAME);
-  const fileName = `${cuid()}-${input.name}`;
+
+  // Replaces () in case someone uploads a file with (1)
+  const fileName = `${cuid()}-${input.name.replace(/ *\([^)]*\) */g, '')}`;
   const file = bucket.file(fileName);
 
   const base64EncodedImageString = input.content.replace(/^data:image\/\w+;base64,/, '');
